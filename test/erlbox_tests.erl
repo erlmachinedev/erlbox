7118974a5f21bbf32650369407be52ae541df1eb
erlmachinedev <erlmachinedev@gmail.com>
2024-09-21 15:02:17 +0000

    Compacted test

--- test/erlbox_tests.erl
+++ test/erlbox_tests.erl
@@ -4,27 +4,31 @@
 -include_lib("eunit/include/eunit.hrl").
 
 erlbox_test_() ->
-  [ {"The format (failure)", fun () -> R0 = erlbox:failure(),
-                                        R1 = erlbox:failure(error),
-                                        R2 = erlbox:failure(error, reason),
-                                        R3 = erlbox:failure(error, reason, _State = #{}),
+  State = #{},
+  Reason = {test, _Stack = []},
+  
+  Acc = #{},
+  
+  [ {"Format (failure)", fun () -> R0 = erlbox:failure(),
+                                   R1 = erlbox:failure(error),
+                                   R2 = erlbox:failure(error, Reason),
+                                   R3 = erlbox:failure(error, Reason, State),
                                         
-                                        Fun = fun erlbox:is_failure/1,
-                                        Res = [R0, R1, R2, R3],
+                                   Fun = fun erlbox:is_failure/1,
                                         
-                                        _ = inspect(Fun, Res)
-                              end },
+                                   _ = inspect(Fun, _Res = [R0, R1, R2, R3])
+                         end 
+    },
 
-    {"The format (success)", fun () -> R0 = erlbox:success(),
-                                        R1 = erlbox:success(ok),
-                                        R2 = erlbox:success(ok, _State0 = #{}),
-                                        R3 = erlbox:success(ok, _State1 = #{}, _Acc = #{}),
+    {"Format (success)", fun () -> R0 = erlbox:success(),
+                                   R1 = erlbox:success(ok),
+                                   R2 = erlbox:success(ok, State),
+                                   R3 = erlbox:success(ok, State, Acc),
 
-                                        Fun = fun erlbox:is_success/1,
-                                        Res = [R0, R1, R2, R3],
-                                        
-                                        _ = inspect(Fun, Res)
-                              end 
+                                   Fun = fun erlbox:is_success/1,
+
+                                   _ = inspect(Fun, _Res = [R0, R1, R2, R3])
+                         end 
     }
   ].
 
