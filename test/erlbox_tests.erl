99367d59c59699a349c61e466ae5c8d0c634ba5d
erlmachinedev <erlmachinedev@gmail.com>
2024-09-21 14:49:49 +0000

    Compacted test

--- test/erlbox_tests.erl
+++ test/erlbox_tests.erl
@@ -4,26 +4,31 @@
 -include_lib("eunit/include/eunit.hrl").
 
 erlbox_test_() ->
-    { foreach,
-      fun() -> ok
-      end,
+  [ {"The format (failure)", fun () -> R0 = erlbox:failure(),
+                                        R1 = erlbox:failure(error),
+                                        R2 = erlbox:failure(error, reason),
+                                        R3 = erlbox:failure(error, reason, _State = #{}),
+                                        
+                                        Fun = fun erlbox:is_failure/1,
+                                        Res = [R0, R1, R2, R3],
+                                        
+                                        _ = inspect(Fun, Res)
+                              end },
 
-      fun (_) -> ok
-      end,
-      [ {"The format of failure", fun () -> R0 = erlbox:failure(),
-                                            R1 = erlbox:failure(error),
-                                            R2 = erlbox:failure(error, reason),
-                                            R3 = erlbox:failure(error, reason, _State = #{}),
+    {"The format (success)", fun () -> R0 = erlbox:success(),
+                                        R1 = erlbox:success(ok),
+                                        R2 = erlbox:success(ok, _State0 = #{}),
+                                        R3 = erlbox:success(ok, _State1 = #{}, _Acc = #{}),
 
-                                            [true = erlbox:is_failure(R) || R <- [R0, R1, R2, R3]]
-                                  end },
+                                        Fun = fun erlbox:is_success/1,
+                                        Res = [R0, R1, R2, R3],
+                                        
+                                        _ = inspect(Fun, Res)
+                              end 
+    }
+  ].
 
-        {"The format of success", fun () -> R0 = erlbox:success(),
-                                            R1 = erlbox:success(ok),
-                                            R2 = erlbox:success(ok, _State0 = #{}),
-                                            R3 = erlbox:success(ok, _State1 = #{}, _Acc = #{}),
 
-                                            [true = erlbox:is_success(R) || R <- [R0, R1, R2, R3]]
-                                  end }
-      ]
-    }.
+inspect(Fun, Res) ->
+  [true = Fun(X) || X <- Res].
+ 
